306c781e4de015793192bd2aeda6e8bc8f742e69
Shubh <shughit@gmail.com>
2022-01-04 18:01:14 +0530

    added new content type

--- config/database.js
+++ config/database.js
@@ -19,3 +19,23 @@ module.exports = ({ env }) => ({
   },
 });
  
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.tamft.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'ASGbackend'),
+//         username: env('DATABASE_USERNAME', 'Admin'),
+//         password: env('DATABASE_PASSWORD', 'asg22PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
